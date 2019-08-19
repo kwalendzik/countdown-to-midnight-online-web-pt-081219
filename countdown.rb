8b117809b741db5147386324de36c4d18f1e4916
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-08-19 15:14:09 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,17 @@
-def countdown(num)
-while num > 0
- puts "#{num} SECOND(S)!"
- num -= 1
- end
- puts "HAPPY NEW YEAR!"
+def countdown(seconds)
+  seconds = 10
+  while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(second)
+  second = 10
+  while second > 0
+  puts "#{second} SECOND(S)!"
+  second -= 1
+  sleep 1
+end
 end
